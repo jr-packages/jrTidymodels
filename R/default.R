fe95ba795933f787992d9e298b0859f1aa3a45c3
JohnMcIntyreJR <john@jumpingrivers.com>
2020-09-30 13:19:38 +0100

    Change default to Default

--- R/default.R
+++ R/default.R
@@ -1,13 +1,13 @@
-#' The default data set
+#' The Default data set
 #'
 #' Contains loan data from the from the {ISLR} package. It contains
 #' information on credit card debt for 10,000 people. Variables include
 #' whether they've defaulted, their yearly income, current credit card
 #' balance and whether they're a student.
-#' @name default
+#' @name Default
 #' @docType data
-#' @usage data(default)
+#' @usage data(Default)
 #' @return A data frame
 #' @examples
-#' data(default)
+#' data(Default)
 NULL
