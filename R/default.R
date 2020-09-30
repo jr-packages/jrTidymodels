d4e0ebaadb7780d80a17e0e79123bde97d9ae270
JohnMcIntyreJR <john@jumpingrivers.com>
2020-09-30 13:02:42 +0100

    Add default data to pkg

---
+++ R/default.R
@@ -0,0 +1,13 @@
+#' The default data set
+#'
+#' Contains loan data from the from the {ISLR} package. It contains
+#' information on credit card debt for 10,000 people. Variables include
+#' whether they've defaulted, their yearly income, current credit card
+#' balance and whether they're a student.
+#' @name default
+#' @docType data
+#' @usage data(default)
+#' @return A data frame
+#' @examples
+#' data(default)
+NULL
